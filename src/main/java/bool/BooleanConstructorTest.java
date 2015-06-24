adca41f7e05a211dbaf20415ac90d2d3b043a902
stran <stran@redhat.com>
2015-06-24 12:59:47 -0400

    Something random I did

--- src/main/java/bool/BooleanConstructorTest.java
+++ src/main/java/bool/BooleanConstructorTest.java
@@ -8,10 +8,15 @@ public class BooleanConstructorTest {
         t = null;
         Boolean b = null;
         
+        String n = null;
+        
         System.out.println("true");
         System.out.println("false");
         System.out.println("true ");
+        System.out.println(t);
         System.out.println(new Boolean(null));
+        System.out.println(new Boolean(n));
+
 
 
         System.out.println(new Boolean(b));
